805752751563c39b631a25a8c3150e6bd4c8cacc
nobody <nobody@gmail.com>
2018-05-11 23:17:35 +0300

    animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('clickedDiv', [
+      state('start', style({
+        backgroundColor: 'yellow',
+        width: '150px',
+        height: '150px'
+      })),
+      state('end', style({
+        backgroundColor: 'green',
+        width: '300px',
+        height: '300px'
+      })),
+      transition('start => end', animate(1500)),
+      transition('end => start', animate('800ms 0.5s ease-out'))
+    ]),
+    trigger('animateShowBlock', [
+      state('close', style({
+        opacity: 0,
+        backgroundColor: 'red'
+      })),
+      state('open', style({
+        opacity: 1,
+        backgroundColor: 'green'
+      })),
+      transition('close <=> open', animate(3000)),
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'app';
+  isMenuOpen = false;
+
+  clickedDivState = 'start';
+
+  changeDivState() {
+    this.clickedDivState = 'end';
+    setTimeout(() => {
+      this.clickedDivState = 'start';
+    }, 3000);
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
